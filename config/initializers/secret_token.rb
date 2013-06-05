436f628d656b37f6a64c7b7b3fd872ff04c49494
Kirk Okada <kirkokada@gmail.com>
2013-06-05 17:30:01 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Invitor::Application.config.secret_key_base = '9bb789bb022f5fb80af7d6e364e1002f90a883fd8851f0d2319cd9d20b5e3cee3331bafc95090418f6a2c030d06c4519eb69d6eba497513ee1b7a37c86572fa6'
